0d10bdb9d18c86d188f71281fe2efd10eb05e7bf
igorkf <igorkuivjogi@hotmail.com>
2025-05-13 17:59:13 -0500

    add first lgbm files

---
+++ 4-train_lgbm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=lgbm
+#SBATCH --output=logs/train_lgbm.out
+#SBATCH --partition=cloud72
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+
+## configs 
+module purge
+module load python
+source myenv/bin/activate
+
+## copy needed files to /scratch
+cd /scratch/$SLURM_JOB_ID
+mkdir -p data src logs output
+scp $SLURM_SUBMIT_DIR/output/x*.csv output/
+scp $SLURM_SUBMIT_DIR/output/y*.csv output/
+scp $SLURM_SUBMIT_DIR/src/*.py src/
+
+#####################################################
+## run tasks
+#####################################################
+python -u src/train_lgbm.py > logs/train_lgbm.py
+#####################################################
+
+## copy needed output files to /home
+scp output/pred.csv $SLURM_SUBMIT_DIR/output/
+scp logs/logs/train_lgbm.py $SLURM_SUBMIT_DIR/logs/
---
+++ src/train_lgbm.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import lightgbm as lgbm
+from sklearn.metrics import root_mean_squared_error
+
+
+# load data
+xtrain = pd.read_csv("output/xtrain_fl_0.csv")
+ytrain = pd.read_csv("output/ytrain_fl_0.csv")
+xval = pd.read_csv("output/xval_fl_0.csv")
+yval = pd.read_csv("output/yval_fl_0.csv")
+
+# hyperparameters
+params = {
+    "lambda_l1": 0.00019843354389067637,
+    "lambda_l2": 2.0952311071236456e-05,
+    "num_leaves": 81,
+    "feature_fraction": 0.6702415073506178,
+    "bagging_fraction": 0.7580106563644224,
+    "bagging_freq": 2,
+    "min_child_samples": 90,
+}
+
+# fit model
+model = lgbm.LGBMRegressor(
+    # **params,
+    random_state=42,
+    max_depth=5,
+    n_estimators=100,
+    verbose=-1,
+)
+model.fit(xtrain, ytrain)
+
+# predict
+ypred_train = model.predict(xtrain)
+ypred = model.predict(xval)
+
+# calculate importance
+df_imp = pd.DataFrame()
+df_imp["score"] = model.feature_importances_
+df_imp["name"] = model.feature_name_
+df_imp = df_imp.sort_values("score", ascending=[False], ignore_index=True)
+print("df imp (top 20):")
+print(df_imp.head(20))
+df_imp.to_csv("output/importance.csv", index=False)
+
+# evaluate
+xval["ytrue"] = yval
+xval["ypred"] = ypred
+rmse = root_mean_squared_error(xval["ytrue"], xval["ypred"])
+corr = (
+    xval[["ytrue", "ypred"]]
+    .groupby("Env")
+    .corr()
+    .iloc[::2, 1]
+    .droplevel(1)
+    .sort_values()
+)
+print("yval std:", float(yval.std()))
+print("global rmse:", float(rmse))
+print("env corr:", corr)
+print("global corr:", float(xval[["ytrue", "ypred"]].corr().iloc[1, 0]))
+print("mean env corr:", float(corr.mean()))
+print("mean env corr summary:\n", corr.describe())
+
+# check distribution train/val/test
+dist_pred = pd.DataFrame()
+dist_pred["train"] = pd.DataFrame(ypred_train).describe()
+dist_pred["val"] = pd.DataFrame(ypred).describe()
+print(dist_pred.round(3))
+
+# write predictions
+xval[["Env", "Hybrid", "ytrue", "ypred"]].to_csv("output/pred.csv", index=False)
